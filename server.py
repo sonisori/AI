814d570620c703fcce49710caa66d46245082349
Donghyun Lee <leedonghyun022@gmail.com>
2024-11-03 17:28:15 +0900

    feat: cors 허용 추가

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import tensorflow as tf
 import numpy as np
 
 app = Flask(__name__)
+CORS(app)
 
 # @app.route('/')
 # def hello_world():
