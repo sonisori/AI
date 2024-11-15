de05274973a09f71b7d37c6eafb6fe33f42a508b
doeunkim <blankbird@hanmail.net>
2024-11-16 07:43:52 +0900

    rename, refactor: sign_to_words
    이전에 flask+socket으로 만든 수어를 단어로 변환해주는 서버를  sign_to_words 폴더로 따로 구분

--- src/server/sign_to_words/server.py
+++ src/server/sign_to_words/server.py
@@ -2,14 +2,33 @@ from flask import Flask, session
 from flask_socketio import SocketIO, emit
 import numpy as np
 import tensorflow as tf
-from preprocess_data import *
 
 actions = ['hello','bye']
 seq_length = 30
 
-model = tf.keras.models.load_model('models/model.keras')
+model = tf.keras.models.load_model('../../models/model.keras')
 
+def preprocess_data_server(res):
+    joint = np.array([[lm['x'], lm['y'], lm['z']] for lm in res])
 
+    # Compute angles between joints
+    v1 = joint[[0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15, 0, 17, 18, 19]]  # Parent joint
+    v2 = joint[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]]  # Child joint
+    v = v2 - v1  # [20, 3]
+
+    # Normalize v
+    v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]  # v의 길이로 나눠줌
+
+    # Get angle using arcos of dot product
+    angle = np.arccos(np.einsum('nt,nt->n',
+                                v[[0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14, 16, 17, 18], :],
+                                v[[1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19], :]))  # [15,] 15개의 각도 구함
+
+    angle = np.degrees(angle)  # Convert radian to degree
+
+    d = np.concatenate([joint.flatten(), angle])  # data concat
+
+    return d
 
 # Flask 및 SocketIO 설정
 app = Flask(__name__)
